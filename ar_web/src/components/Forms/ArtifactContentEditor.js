58048e267c504e9596e5b1f2429a63f8005f1d54
Truong <55469561+FCBTruong@users.noreply.github.com>
2023-04-07 19:28:43 +0700

    add editorjs, and custom

--- ar_web/src/components/Forms/ArtifactContentEditor.js
+++ ar_web/src/components/Forms/ArtifactContentEditor.js
@@ -1,47 +1,55 @@
-import { YoptaEditor } from "yopta-editor";
 import { useState, useEffect } from "react";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import Paragraph from "components/customize-editorjs/paragraph";
+import Input from "reactstrap/lib/Input";
 
-import user from "apis/user";
-import s from "./styles.module.scss";
-import "yopta-editor/dist/index.css";
-
-const onChangeMedia = async (file, type) => {
-  const { url, data } = await user.uploadToCloudinary(file, type);
-  return { url, options: data };
-};
-
-const media = {
-  imageProps: {
-    onChange: (file) => onChangeMedia(file, "image"),
-    accept: "image/*",
-  },
-  videoProps: {
-    onChange: (file) => onChangeMedia(file, "video"),
-  },
-};
 
 function ArtifactContentEditor(props) {
   console.log("artifact...---", props.artifact);
-  const [editorValue, setEditorValue] = useState([]);
 
-  useEffect(() => {
-    props.artifact.data = editorValue;
+  new EditorJS({
+    /**
+     * Id of Element that should contain the Editor
+     */
+    holder: "editorjs",
+
+    /**
+     * Available Tools list.
+     * Pass Tool's class or Settings object for each Tool you want to use
+     */
+    tools: {
+      header: {
+        class: Header,
+        inlineToolbar: ["link"],
+      },
+      paragraph: {
+        class: Paragraph,
+        inlineToolbar: true,
+        config: {
+          spellcheck: false,
+        },
+      },
+      list: {
+        class: List,
+        inlineToolbar: true,
+      },
+    },
+    onReady: () => {
+      console.log("Editor.js is ready to work!");
+    },
+    onChange: (api, event) => {
+      console.log("Now I know that Editor's content changed!", event);
+    },
+    placeholder: 'Let`s write an awesome story!'
   });
 
-  return (
-    <div className={s.container} spellcheck="false">
-      <YoptaEditor
-        value={editorValue}
-        onChange={(val) => setEditorValue(val)}
-        className={s.editor}
-        media={media}
-        shouldStoreInLocalStorage={{ name: "localStorage-name" }}
-        components={[
-          
-        ]}
-      />
-    </div>
-  );
+  useEffect(() => {
+    //props.artifact.data = editorValue;
+  });
+
+  return <div id="editorjs" />;
 }
 
 export default ArtifactContentEditor;
